1e6700c6baa250882b29453581697c282681a8d8
Ken Ohata <11092425+ohataken@users.noreply.github.com>
2024-12-31 22:07:00 +0900

    bundle gem cards

---
+++ cards/lib/cards.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cards/version"
+
+module Cards
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ cards/lib/cards/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Cards
+  VERSION = "0.1.0"
+end
