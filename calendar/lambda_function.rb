fe3a68212139ab0ba23859c31aa495adda09d0ca
Ken Ohata <11092425+ohataken@users.noreply.github.com>
2024-12-31 23:09:50 +0900

    update LambdaFunction::Handler.process

--- calendar/lambda_function.rb
+++ calendar/lambda_function.rb
@@ -1,10 +1,21 @@
+require "lib/calendar"
 require "io/console"
 require "stringio"
 
 module LambdaFunction
   class Handler
     def self.process(event:,context:)
-      "Hello from Lambda!"
+      id = event["queryStringParameters"]["id"]
+      calendar = Calendar::Calendar.new id
+
+      {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: {
+          "Cache-Control": "public, max-age=3600"
+        },
+        body: calendar.to_hash.to_json
+      }
     end
   end
 end
