f5229617bebe4039e16631342885a26b74607f7b
Ken Ohata <11092425+ohataken@users.noreply.github.com>
2025-01-01 19:13:54 +0900

    bundle exec standardrb --fix

--- calendar/lib/calendar/card.rb
+++ calendar/lib/calendar/card.rb
@@ -12,46 +12,45 @@ module Calendar
     end
 
     def anchor
-      @anchor ||= @node.at_css('a')
+      @anchor ||= @node.at_css("a")
     end
-  
+
     def day_node
-      @day_node ||= @node.at_css('a > div.m_calCardHead > div.m_calCardDay')
+      @day_node ||= @node.at_css("a > div.m_calCardHead > div.m_calCardDay")
     end
-  
+
     def day
       day_node&.children&.at(0)&.text&.strip.to_i
     end
-  
+
     def image_node
-      @image_node ||= @node.at_css('a > div.m_calCardHead > div.m_calCardImage > img')
+      @image_node ||= @node.at_css("a > div.m_calCardHead > div.m_calCardImage > img")
     end
-  
+
     def text_node
-      @text_node ||= @node.at_css('a > div.m_calCardBody > p.m_calCardText')
+      @text_node ||= @node.at_css("a > div.m_calCardBody > p.m_calCardText")
     end
-  
+
     def category_node
-      @category_node ||= @node.at_css('a > div.m_calCardBody > span.m_calCardCat')
+      @category_node ||= @node.at_css("a > div.m_calCardBody > span.m_calCardCat")
     end
-  
+
     def to_hash
       {
         anchor: {
-          href: anchor && anchor['href']
+          href: anchor && anchor["href"]
         },
         day:,
         image: {
-          src: image_node && image_node['src']
+          src: image_node && image_node["src"]
         },
         body: {
-          children: text_node&.children&.map { |e| { text: e.text } }
+          children: text_node&.children&.map { |e| {text: e.text} }
         },
         category: {
           content: category_node&.content
         }
       }
     end
-  
   end
 end
