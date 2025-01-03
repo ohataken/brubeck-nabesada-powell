c050506b0765fa41fb939345e90f04415f4f2bf7
Ken Ohata <11092425+ohataken@users.noreply.github.com>
2025-01-03 19:43:11 +0900

    update LambdaFunction::Handler.process

--- cards/lambda_function.rb
+++ cards/lambda_function.rb
@@ -1,10 +1,23 @@
+require "lib/cards"
 require "io/console"
 require "stringio"
 
 module LambdaFunction
   class Handler
     def self.process(event:, context:)
-      "Hello from Lambda!"
+      id = event["queryStringParameters"]["id"]
+      date = Date.parse(id)
+      calendar = Cards::Calendar.new date
+      card = calendar.cards.find { |c| c.date == date }
+
+      {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: {
+          "Cache-Control": "public, max-age=86400"
+        },
+        body: card.to_hash.to_json
+      }
     end
   end
 end
