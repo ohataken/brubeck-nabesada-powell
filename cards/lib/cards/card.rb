8842ac7a87fae304f096919ad3abc647265d57a5
Ken Ohata <11092425+ohataken@users.noreply.github.com>
2025-01-03 19:36:54 +0900

    reset Cards::Card

--- cards/lib/cards/card.rb
+++ cards/lib/cards/card.rb
@@ -2,32 +2,8 @@
 
 require "uri"
 require "net/http"
-require "nokogiri"
 
 module Cards
   class Card
-    def hostname
-      ENV.fetch("CARD_HOSTNAME", "example.com")
-    end
-
-    def uri_string
-      "https://#{hostname}/cards"
-    end
-
-    def uri
-      @uri ||= URI.parse(uri_string)
-    end
-
-    def body
-      @body ||= Net::HTTP.get(uri)
-    end
-
-    def encoded_body
-      body.force_encoding(Encoding::UTF_8)
-    end
-
-    def document
-      @document ||= Nokogiri::HTML(encoded_body)
-    end
   end
 end
