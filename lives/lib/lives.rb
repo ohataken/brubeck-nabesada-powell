5afb6dc7ad56f8dc24a0f020a41594147e637e46
ohataken <11092425+ohataken@users.noreply.github.com>
2025-01-04 19:24:25 +0900

    bundle gem lives

---
+++ lives/lib/lives.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lives/version"
+
+module Lives
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lives/lib/lives/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Lives
+  VERSION = "0.1.0"
+end
