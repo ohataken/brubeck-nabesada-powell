83a4d8df911e4213cb98c393f40eea903f306363
ohataken <11092425+ohataken@users.noreply.github.com>
2025-01-04 20:03:18 +0900

    update LambdaFunction::Handler.process

--- lives/lambda_function.rb
+++ lives/lambda_function.rb
@@ -1,10 +1,21 @@
+require "lib/lives"
 require "io/console"
 require "stringio"
 
 module LambdaFunction
   class Handler
     def self.process(event:, context:)
-      "Hello from Lambda!"
+      id = event["queryStringParameters"]["id"]
+      live = Lives::Live.new id
+
+      {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: {
+          "Cache-Control": "public, max-age=86400"
+        },
+        body: live.to_hash.to_json
+      }
     end
   end
 end
